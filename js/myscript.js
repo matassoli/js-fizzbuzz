bb3d8a03f328e87467f83d7d09edeeb0ca1da75b
Nicolò Matassoli <nico.matassoli@gmail.com>
2021-03-02 16:16:59 +0100

    fix

--- js/myscript.js
+++ js/myscript.js
@@ -1,8 +1,10 @@
 for (var i = 1; i < 101; i++) {
-  if (i % 3 == 0) {
+  if (i % 3 == 0 && i % 5 == 0) {
+    console.log("FizzBuzz");
+  } else if (i % 3 == 0) {
     console.log("Fizz");
   } else if (i % 5 == 0) {
     console.log("Buzz");
-  }
-  console.log(i);
+  } else
+    console.log(i);
 }
